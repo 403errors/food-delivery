d48badd7a999883babb415a38908b4f80b0dae8b
Sameer Verma <na21b061@smail.iitm.ac.in>
2025-02-01 17:55:12 +0530

    add footer and app download components; enhance food display with category filtering and cart functionality

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -3,14 +3,16 @@ import './FoodDisplay.css'
 import { StoreContext } from '../../context/StoreContext'
 import FoodItem from '../FoodItem/FoodItem'
 
-const FoodDisplay = () => {
+const FoodDisplay = ({category}) => {
     const { food_list } = useContext(StoreContext)
   return (
     <div className='food-display' id='food-display'>
       <h2>Top dishes near you</h2>
       <div className="food-display-list">
         {food_list.map((food, index) => {
-            return <FoodItem key={index} id={food._id} name={food.name} description={food.description} price={food.price} image={food.image} />
+            if (category === 'All' || category === food.category){ 
+                return <FoodItem key={index} id={food._id} name={food.name} description={food.description} price={food.price} image={food.image} />
+            }  
         })}
       </div>
     </div>
