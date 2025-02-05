3b900164d4ffae82cedc5a9d041b03979b02bd54
Sameer Verma <na21b061@smail.iitm.ac.in>
2025-02-05 13:45:44 +0530

    .env configure

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,9 @@ import cors from 'cors';
 import { connectDB } from './config/db.js';
 import foodRouter from './routes/foodRoute.js';
 
+import dotenv from "dotenv";
+dotenv.config();
+
 // app config
 const app = express();
 const port = process.env.PORT || 4000;
