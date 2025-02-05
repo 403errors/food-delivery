bd1110220c9dd36accd8078a19709e29613c3ee2
Sameer Verma <na21b061@smail.iitm.ac.in>
2025-02-05 13:02:00 +0530

    security patch

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('').then(() => {console.log('Connected to MongoDB')});
+    await mongoose.connect(process.env.MONGO_URI).then(() => {console.log('Connected to MongoDB')});
 }
