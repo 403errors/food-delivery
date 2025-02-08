a4992223a423ce5f462aabc0427d839a555cbcd8
Sameer Verma <na21b061@smail.iitm.ac.in>
2025-02-08 12:27:09 +0530

    Add order verification and user orders functionality; implement Verify and MyOrders components

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,9 +1,11 @@
 import expres from 'express';
 import authMiddleware from '../middleware/auth.js';
-import { placeOrder } from '../controllers/orderController.js';
+import { placeOrder, userOrders, verifyOrder } from '../controllers/orderController.js';
 
 const orderRouter = expres.Router();
 
-orderRouter.post('/place', authMiddleware, placeOrder)
+orderRouter.post('/place', authMiddleware, placeOrder);
+orderRouter.post('/verify', verifyOrder);
+orderRouter.post("/userOrders", authMiddleware, userOrders);
 
 export default orderRouter;
